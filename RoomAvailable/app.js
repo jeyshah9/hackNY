666e003d00568dce710a1b27d4e1320e14fc1c0e
Jay Shah <jeyshah9@gmail.com>
2016-04-03 00:51:29 -0400

    Both Sheets Updated
    Inserting Into MongoDB Works for both Sheets

--- RoomAvailable/app.js
+++ RoomAvailable/app.js
@@ -9,6 +9,8 @@ var http = require('http');
 var Spreadsheet = require('edit-google-spreadsheet');
 var async = require('async');
 var path = require('path');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/hackNY');
 
 var app = express();
 // all environments
@@ -31,13 +33,29 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 
+var RoomSchema = mongoose.Schema({
+    roomNumber: String,
+    roomCapacity: String,
+    roomNotes: String
+
+});
+
+var PersonDataSchema = mongoose.Schema({
+    Name: String,
+    PhoneNumber: String,
+    occupied:String
+});
+
+var Rooms = mongoose.model('Rooms', RoomSchema);
+var Persons = mongoose.model('Person',PersonDataSchema);
+
 app.get('/spreadsheet', function (req, res) {
     var GoogleSpreadsheet = require("google-spreadsheet");
     
     
     // spreadsheet key is the long id in the sheets URL
     var doc = new GoogleSpreadsheet('1HaYrc6H9-EAKuCWDY6o0q2wGz91pH9JE57BYEYew844');
-    var sheet1,sheet2;
+    var sheet1,sheet2,row1Count,row2Count;
     
     async.series([
         function setAuth(step) {
@@ -51,21 +69,92 @@ app.get('/spreadsheet', function (req, res) {
                 console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
                 sheet1 = info.worksheets[0];
                 sheet2 = info.worksheets[1];
-                console.log('sheet 1: ' + sheet1.title + ' '+sheet1.rowCount  + 'x' + sheet1.colCount);
-
-                step();
+                row1Count = sheet1.rowCount;
+                row2Count= sheet2.rowCount;
+                step( );
             });
         } ,
         function workingWithRows(step) {
             // google provides some query options
-            sheet1.getRows({
-                offset: 1,
-                limit: 20,
-                orderby: 'col2'
-            }, function (err, row) {
+            
+            sheet1.getCells({
+                'min-row': 1, 
+                'return-empty': false
+            }, function (err, cells) {
+                var jsonData = {};
+                var i = 0;
+                cells.forEach(function (entry) {
+                    if (i == 0) {
+                        jsonData['roomNumber'] = entry.value + "";
+                         
+                    } if (i == 1) {
+                        jsonData['roomCapacity'] = entry.value;
+                        
+                    }
+                    if (i == 2) {
+                        jsonData['roomNotes'] = entry.value;
+                         
+                    }
+                    i++;
+                    if (i == 3) {
+                        i = 0;
+                        console.log('%j', jsonData);
+                         
+                        var saveDb = new Rooms(jsonData);
+                        saveDb.save(function (err, save) {
+                            if (err) {
+                                console.log(err);
+                            }
+                           // console.log(save.roomNotes);
+                        }); 
+                       
+                    }
+                })
+            }
+            );
+            
+            
+            
+            
+            
+            
+
                 
-                console.log(">>> %j",row[0].roomcapacity);
+                sheet2.getCells({
+                    'min-row': 1, 
+                    'return-empty': false
+                }, function (err, cells) {
+                    var jsonData = {};
+                    var i = 0;
+                    cells.forEach(function (entry) {
+                        if (i == 0) {
+                            jsonData['Name'] = entry.value + "";
+                            
+      
+                        } if (i == 1) {
+                            jsonData['PhoneNumber'] = entry.value+"";
+                            jsonData['occupied'] = 0 + "";
+                        }
+                         
+                        i++;
+                        if (i == 2) {
+                            i = 0;
+                            console.log('%j', jsonData);
+                         
+                            var saveDb = new Persons(jsonData);
+                            saveDb.save(function (err, save) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                console.log(save.PhoneNumber);
+                            }); 
+                       
+                        }
+                     
+                    });
+                
             })
+
         }
 
     ]);
