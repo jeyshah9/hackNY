0cd992e2ad63aeb2cea961121c981c0f212a6ed8
Jay Shah <jeyshah9@gmail.com>
2016-04-02 21:43:18 -0400

    Updates1
    Values of Room Capacity

--- RoomAvailable/app.js
+++ RoomAvailable/app.js
@@ -6,13 +6,15 @@
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
+var Spreadsheet = require('edit-google-spreadsheet');
+var async = require('async');
 var path = require('path');
 
 var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -28,9 +30,52 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/about', routes.about);
-app.get('/contact', routes.contact);
 
+app.get('/spreadsheet', function (req, res) {
+    var GoogleSpreadsheet = require("google-spreadsheet");
+    
+    
+    // spreadsheet key is the long id in the sheets URL
+    var doc = new GoogleSpreadsheet('1HaYrc6H9-EAKuCWDY6o0q2wGz91pH9JE57BYEYew844');
+    var sheet1,sheet2;
+    
+    async.series([
+        function setAuth(step) {
+            // see notes below for authentication instructions!
+            var creds = require('./key.json');
+            
+            doc.useServiceAccountAuth(creds, step);
+        },
+        function getInfoAndWorksheets(step) {
+            doc.getInfo(function (err, info) {
+                console.log('Loaded doc: ' + info.title + ' by ' + info.author.email);
+                sheet1 = info.worksheets[0];
+                sheet2 = info.worksheets[1];
+                console.log('sheet 1: ' + sheet1.title + ' '+sheet1.rowCount  + 'x' + sheet1.colCount);
+
+                step();
+            });
+        } ,
+        function workingWithRows(step) {
+            // google provides some query options
+            sheet1.getRows({
+                offset: 1,
+                limit: 20,
+                orderby: 'col2'
+            }, function (err, row) {
+                
+                console.log(">>> %j",row[0].roomcapacity);
+            })
+        }
+
+    ]);
+    
+    
+    
+    
+    res.end();
+});
+ 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
--- RoomAvailable/routes/index.js
+++ RoomAvailable/routes/index.js
@@ -4,13 +4,7 @@
  */
 
 exports.index = function (req, res) {
-    res.render('index', { title: 'Express', year: new Date().getFullYear() });
+    res.render('index' );
 };
 
-exports.about = function (req, res) {
-    res.render('about', { title: 'About', year: new Date().getFullYear(), message: 'Your application description page' });
-};
-
-exports.contact = function (req, res) {
-    res.render('contact', { title: 'Contact', year: new Date().getFullYear(), message: 'Your contact page' });
-};
+ 
